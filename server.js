a4c437deb657bae69c826d0a83573eb04238e54d
wilsonft <wilsonft83@gmail.com>
2017-11-02 01:16:24 -0500

    feat(socketio): add messages socket

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(express.static('./public'));
 
 io.on('connection', (socket) => {
     console.log('socket connection', socket.id);
-    
+
+    socket.emit('messages', messages);
     socket.on('new-message', (data) => {
         messages.push(data);
         io.sockets.emit('messages', messages);
